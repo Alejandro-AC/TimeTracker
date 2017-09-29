284785ce59d368a67917a031e951c81471f2818a
Marc <marc.martias@e-campus.uab.cat>
2017-09-29 15:52:37 +0200

    Diseño Observer.

---
+++ timeTracker/src/projectManagement/Clock.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package projectManagement;
+
+import java.util.Observable;
+
+/** 
+ * @author marcm
+ */
+public class Clock extends Observable {
+
+}
--- timeTracker/src/projectManagement/Interval.java
+++ timeTracker/src/projectManagement/Interval.java
@@ -3,9 +3,17 @@
  */
 package projectManagement;
 
+import java.util.Observable;
+import java.util.Observer;
+
 /** 
  * @author marcm
  */
-public class Interval {
+public class Interval implements Observer {
+
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
