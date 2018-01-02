10ce87503b03f24879667478644c7227a4c97f46
Marc <marc.martias@e-campus.uab.cat>
2018-01-02 17:32:19 +0100

    Problema 2 solucionado. Una Activity que está siendo cronometrada muestra su tiempo total de color Rojo.

--- TimeTracker/app/src/main/java/com/example/joans/timetracker/ActivityListAdapter.java
+++ TimeTracker/app/src/main/java/com/example/joans/timetracker/ActivityListAdapter.java
@@ -8,25 +8,27 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.List;
+import android.support.v4.content.ContextCompat;
 
 /**
  * Adaptador per a les llistes d'Activities.
  * Permet mostrar una imatge que diferencia els tipus d'Activity de cada element (una carpeta per un
  * Project i un full per una Task).
+ * Quan s'està cronometrant una Activity, aquesta mostra el seu temps total de color VERMELL.
  */
 public class ActivityListAdapter extends ArrayAdapter<DadesActivitat> {
 
     private final Activity context;
-    private final List<DadesActivitat> itemname;
+    private final List<DadesActivitat> dadesActivitats;
 
-    public ActivityListAdapter(Activity context, List<DadesActivitat> itemname) {
-        super(context, R.layout.fila_llista_activitats, R.id.text_fila, itemname);
+    public ActivityListAdapter(Activity context, List<DadesActivitat> llistaDadesActivitats) {
+        super(context, R.layout.fila_llista_activitats, R.id.text_fila, llistaDadesActivitats);
         this.context=context;
-        this.itemname=itemname;
+        this.dadesActivitats=llistaDadesActivitats;
     }
 
     @Override
-    public View getView(int posicion, View view, ViewGroup parent){
+    public View getView(int posicio, View view, ViewGroup parent){
 
         LayoutInflater inflater=context.getLayoutInflater();
         View rowView=inflater.inflate(R.layout.fila_llista_activitats,null,true);
@@ -34,11 +36,15 @@ public class ActivityListAdapter extends ArrayAdapter<DadesActivitat> {
         TextView txtTitle = (TextView) rowView.findViewById(R.id.text_fila);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
 
-        txtTitle.setText(itemname.get(posicion).toString());
+        // Canvia el color del temps si l'Activity està sent cronometrada.
+        if (dadesActivitats.get(posicio).isCronometreEngegat()) {
+            txtTitle.setTextColor(ContextCompat.getColor(context, R.color.colorRunning));
+        }
 
-        System.out.println("Posicion: " + posicion);
+        txtTitle.setText(dadesActivitats.get(posicio).toString());
 
-        if (itemname.get(posicion).isProjecte()) {
+        // Mostra la imatge corresponent.
+        if (dadesActivitats.get(posicio).isProjecte()) {
             imageView.setImageResource(R.drawable.ic_project);
         } else {
             imageView.setImageResource(R.drawable.ic_task);
